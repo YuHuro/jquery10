f634c1a9d0e356240a86bb0d0d119958202a2d9c
YuHuro <128969811+YuHuro@users.noreply.github.com>
2023-09-09 14:49:29 +0300

    Update js.js

--- 3/js.js
+++ 3/js.js
@@ -6,6 +6,6 @@ $(btn).on('click', function(){
     if(isNaN(number.value) || input.value === ''){
         return alert('Введите число!')
     }else{
-        return block.innerHTML = '<h1>'+number.value*number.value+'</h1>'
+        block.innerHTML = '<h1>'+number.value*number.value+'</h1>'
     }
 })
