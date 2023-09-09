7f3ed174362213b148c9cfd8f733f15495556654
YuHuro <128969811+YuHuro@users.noreply.github.com>
2023-09-09 14:47:43 +0300

    Update js.js

--- 2/js.js
+++ 2/js.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('#input')
-const button = document.querySelector('#button')
-const block = document.querySelector('#block')
+const input = $('#input')
+const button = $('#button')
+const block = $('#block')
 
 $(button).on('click', function(){
     if(input.value === ''){
@@ -9,6 +9,6 @@ $(button).on('click', function(){
     if(input.value.length < 3){
         return block.innerHTML = '<h1>Не меньше 3-х символов!</h1>'
     }else{
-        return block.innerHTML = '<h1>'+input.value+'</h1>'
+        block.innerHTML = '<h1>'+input.value+'</h1>'
     }
 })
